43d36f2180c73e4484071858ea1da71d6b223e13
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2025-07-20 19:33:13 +0300

    tooltips completed?

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,35 @@
+const hasTooltips = [...document.querySelectorAll('.has-tooltip')];
+
+function eventListeners() {
+  hasTooltips.forEach((el) => {el.addEventListener('click', showTooltip)})
+}
+
+function showTooltip(e) {
+  e.preventDefault();
+  const tooltip = e.target.nextElementSibling;
+  const rect = tooltip.getBoundingClientRect();
+
+  const isActive = tooltip.classList.contains('tooltip_active');
+  hideAllTooltips();
+  if (!isActive) {
+    tooltip.classList.add('tooltip_active');
+  }
+}
+
+function addTooltips() {
+  hasTooltips.forEach((el) => {
+    const tooltip = document.createElement('div')
+    tooltip.classList.add('tooltip');
+    tooltip.textContent = el.title;
+    el.insertAdjacentElement('afterend', tooltip);
+  })
+}
+
+function hideAllTooltips() {
+  document.querySelectorAll('.tooltip').forEach(tooltip => {
+    tooltip.classList.remove('tooltip_active');
+  });
+}
+
+addTooltips();
+eventListeners();
