919092d404158ac9af3dfaa9b420790c5accbef3
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2025-07-21 17:55:02 +0300

    edit addTask

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -7,30 +7,47 @@ function eventListeners() {
 
 function sendData(e) {
   e.preventDefault();
-  if (taskInput.value) addTask(taskInput.value);
+  if (taskInput.value.trim()) {
+    addTask(taskInput.value)
+    taskInput.value = "";
+  }
 }
 
 function addTask(taskText) {
   const tasksList = document.querySelector("#tasks__list");
+  tasksList.insertAdjacentHTML('afterbegin', `
+    <div class="task">
+      <div class="task__title">
+        ${taskText}
+      </div>
+      <a href="#" class="task__remove">&times;</a>
+    </div>
+   `);
 
-  const newTask = document.createElement('div');
-  newTask.className = 'task';
-
-  const taskTitle = document.createElement('div');
-  taskTitle.className = 'task__title';
-  taskTitle.textContent = taskText;
-
-  const taskRemove = document.createElement('a')
-  taskRemove.className = 'task__remove';
-  taskRemove.innerHTML = '&times;';
-  taskRemove.addEventListener('click', (e) => {
+  const taskRemove = tasksList.querySelector(".task__remove");
+  taskRemove.addEventListener("click", (e) => {
     e.preventDefault();
-    newTask.remove();
-  });
+    e.target.closest('.task').remove();
+  })
 
-  newTask.appendChild(taskTitle);
-  newTask.appendChild(taskRemove);
-  tasksList.appendChild(newTask);
+  // const newTask = document.createElement('div');
+  // newTask.className = 'task';
+  //
+  // const taskTitle = document.createElement('div');
+  // taskTitle.className = 'task__title';
+  // taskTitle.textContent = taskText;
+  //
+  // const taskRemove = document.createElement('a')
+  // taskRemove.className = 'task__remove';
+  // taskRemove.innerHTML = '&times;';
+  // taskRemove.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   newTask.remove();
+  // });
+  //
+  // newTask.appendChild(taskTitle);
+  // newTask.appendChild(taskRemove);
+  // tasksList.appendChild(newTask);
 }
 
 eventListeners();
